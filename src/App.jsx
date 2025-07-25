82440f62b17ec7948e1be1dc7f06191b0574fe1a
arashad202 <ahm.rashad.202@gmail.com>
2025-07-25 08:12:07 +0300

    Exercise 1.4
    Place the objects into an array. Modify the
    variable definitions of App and refactor.

--- src/App.jsx
+++ src/App.jsx
@@ -6,38 +6,42 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
-    <p>{props.part} {props.exercise}</p>
+    props.parts.map(part => <p key={part.name}> {part.name} {part.exercises} </p>)
   )
 }
 
 const Total = (props) => {
+  let sum = 0;
+
+  props.parts.forEach(part => sum += part.exercises);
+
   return (
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    <p>Number of exercises {sum}</p>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header name={course}/>
-      <Content part={part1.name} exercise={part1.exercises}/>
-      <Content part={part2.name} exercise={part2.exercises}/>
-      <Content part={part3.name} exercise={part3.exercises}/>
-      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises}/>
+      <Content parts={parts}/>
+      <Total parts={parts}/>
     </div>
   )
 }
